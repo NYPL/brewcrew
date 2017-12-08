8488dc60de05ab7ced7eece9a5e5484eaa2d364e
Darya Chernikhova <daryachernikhova@nypl.org>
2017-12-08 13:50:02 -0500

    prepopulate the db, and run a user select query from a view

--- compute_cycle/urls.py
+++ compute_cycle/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('prepopulate', views.prepopulate, name='prepopulate'),
+    path('sasb_users', views.sasb_users, name='sasb_users'),
 ]
 
