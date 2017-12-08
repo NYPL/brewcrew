9c6353756c42590d7b7d8e146f1054a89eb0b026
Courteney Ervin <courteneyervin@nypl.org>
2017-12-08 10:43:07 -0500

    Use Google OAuth flow

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
-
+from pdb import set_trace
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from oauth2client.client import flow_from_clientsecrets
+
+
+FLOW = flow_from_clientsecrets(
+    'client_secrets.json',
+    scope=[
+        'https://www.googleapis.com/auth/calendar',
+        'https://www.googleapis.com/auth/userinfo.email',
+    ],
+    redirect_uri='http://localhost:8000/accounts/google_callback',
+)
+
 
 def index(request):
-    return HttpResponse("registering urls")
+    authorization_url = FLOW.step1_get_authorize_url()
+    return redirect(authorization_url)
+
+def google_callback(request):
+    error = request.GET.get('error')
+    if error:
+        raise ValueError('We gots prahblems.')
+
+    code = request.GET.get('code')
+    if not code:
+        raise ValueError('No code returned')
+
+    credentials = FLOW.step2_exchange(code)
+    return HttpResponse("hey")
